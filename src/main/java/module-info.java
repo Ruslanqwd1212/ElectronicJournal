2a7ebea8438001700724115b4ec0e12ded475a88
Руслан Жевраков <116262017+RyslanZevrakov@users.noreply.github.com>
2022-12-16 16:01:01 +0300

    Add files via upload

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
-module com.example.electronicjournal {
+module com.example.exampleproject {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.electronicjournal to javafx.fxml;
-    exports com.example.electronicjournal;
+    opens com.example.exampleproject to javafx.fxml;
+    exports com.example.exampleproject;
 }
